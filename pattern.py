6b7d400050d3f74f57a6c16dac1b197222bb0bdf
tjs322 <tjs322@cornell.edu>
2024-08-11 15:38:33 -0400

    Edit the GrowthPattern class to better fit future uses.

--- pattern.py
+++ pattern.py
@@ -1,5 +1,3 @@
-import sqlite3
-import database
 import yfinance as yf
 import numpy as np
 import statistics
@@ -53,33 +51,24 @@ class ResultPattern():
     
     sorted_results = sorted(results_dict.items(), \
                             key=lambda item: item[1], reverse=True)
-
     self.__score_results = sorted_results
 
     return sorted_results
 
   def get_top_ten(self):
+    """
+    Returns the top ten values of the scored results in [self.__score_results]
+    """
     return self.__create_top_ten(self.__score_results)
-
-#Test ResultPattern for Penny Stocks
-# CONN = sqlite3.connect("stock_tickers.db")
-# set_name = "penny_stocks_2024-08-07"
-# penny_set = database.StockTickerDatabase.retrieve_set(CONN, set_name)
-# RP = ResultPattern(penny_set)
-# train_results = RP.top_ten_train('2023-12-31')
-# print(train_results)
-# test_results = RP.top_ten_test('2024-01-01')
-# print(test_results)
-
 
 class GrowthPattern():
   def __init__(self, ticker_set):
     self.__tickers = ticker_set
-    self.__top_ten = None
+    self.__score_results = None
 
   def __find_open(self, stock):
     """
-    Removes all None values from the list and returns the mean of the list.
+    Finds the first [open_index] where the open value is not zero.
     """
     open_index = 0
     while stock['Open'].iloc[open_index] == 0:
@@ -100,8 +89,11 @@ class GrowthPattern():
 
     self.__top_ten = top_ten
 
-  def top_ten_train(self, end_date):
-    #Find the top ten stocks with the highest growth given the end date for the "training" data
+  def score(self, end_date):
+    """
+    Scores all of the stock tickers within the set [self.__tickers] to find
+    the highest growth rates up to [end_date].
+    """    
     growth_dict = {}
     progress = 0
     for ticker in self.__tickers:
@@ -121,46 +113,12 @@ class GrowthPattern():
     
     sorted_results = sorted(growth_dict.items(), \
                             key=lambda item: item[1], reverse=True)
-    self.__create_top_ten(sorted_results)
-
-    return self.__top_ten
-  
-  def top_ten_test(self, start_date):
-    #Test the top ten stocks beginning at the start date and record their frequencies.
-    growth_dict = {}
-    progress = 0
-    for ticker, _ in self.__top_ten:
-      progress += 1
-      print(f'Progress: {progress} of {len(self.__top_ten)}')
-      try:
-        stock = yf.download(ticker, start=start_date)
-        open_index = self.__find_open(stock)
-        if open_index >= len(stock):
-          continue
-        first_open = stock['Open'].iloc[open_index]
-        last_close = stock['Close'].iloc[-1]
-        ticker_growth = (last_close - first_open) / first_open
-        growth_dict[ticker] = ticker_growth
-      except:
-        continue
-    
-    sorted_results = sorted(growth_dict.items(), \
-                            key=lambda item: item[1], reverse=True)
+    self.__score_results = sorted_results
 
     return sorted_results
-  
-  def set_top_ten(self, sorted_results):
-    self.__top_ten = sorted_results
 
   def get_top_ten(self):
-    return self.__top_ten
-  
-#Test GrowthPattern for Penny Stocks
-# CONN = sqlite3.connect("stock_tickers.db")
-# set_name = "penny_stocks_2024-08-07"
-# penny_set = database.StockTickerDatabase.retrieve_set(CONN, set_name)
-# RP = GrowthPattern(penny_set)
-# train_results = RP.top_ten_train('2023-12-31')
-# print(train_results)
-# test_results = RP.top_ten_test('2024-01-01')
-# print(test_results)
+    """
+    Returns the top ten values of the scored results in [self.__score_results]
+    """
+    return self.__create_top_ten(self.__score_results)
